6407c080636c86964c6ba9e50ec4ae99ce43abc6
lihaijiao <lihaijiao@cloudin.ren>
2016-04-17 21:47:15 +0800

    完善登录逻辑

--- mbank/Application/Home/Conf/config.php
+++ mbank/Application/Home/Conf/config.php
@@ -14,7 +14,7 @@ return array(
 
 	//数据库配置信息
 	'DB_TYPE'   => 'mysql', // 数据库类型
-	'DB_HOST'   => '223.202.85.115', // 服务器地址
+	'DB_HOST'   => '127.0.0.1', // 服务器地址
 	'DB_NAME'   => 'mbank', // 数据库名
 	'DB_USER'   => 'root', // 用户名
 	'DB_PWD'    => 'lihaijiao', // 密码
--- mbank/Application/Home/Controller/LoginController.class.php
+++ mbank/Application/Home/Controller/LoginController.class.php
@@ -5,6 +5,20 @@ use Think\Controller;
 class LoginController extends Controller{
 
 	public function index(){
+		$m_bank = json_decode($_POST['mBank'],true);
+		if(IS_POST){
+			$tb_user = M('M_user');
+			$where['email'] = $m_bank['username'];
+			$user = $tb_user->where($where)->select();
+			if($user['password']==$m_bank['password']){
+				myjson(200,'success','login',$user);
+			}else{
+				myjson(500,'密码错误','login');
+			}
+
+		}else{
+			myjson(500,'error-get','login');
+		}
 		$array = array(
 			'data'=>array(
 				'id'=>1,
